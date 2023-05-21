25966eacb85bbbd6fd743c27b73551d22195185a
Nafis Ahmed <nafisahmed@iut-dhaka.edu>
2023-05-21 08:57:20 +0600

    module finished

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="home">
+    <h3>{{ counterData.title }} :</h3>
     <div>
       <button class="btn" @click="decrement">-</button>
-      <span class="counter">{{ counter }}</span>
+      <span class="counter">{{ counterData.count }}</span>
       <button class="btn" @click="increment">+</button>
+    </div>
+    <div class="edit">
+      <h4>{{inp}}</h4>
+      <input v-model="counterData.title" type="text" />
     </div>
   </div>
 </template>
@@ -26,13 +31,43 @@ export default {
   }
 }
 </script> -->
+
+<!-- Composition API v1 -->
+
+<!-- <script>
+  export default{
+    write everything in setup function
+    setup() {
+      const counter = Vue.ref(0)
+      const increment = () => counter.value++
+      const decrement = () => counter.value--
+      return {
+        counter,
+        increment,
+        decrement
+      }
+    }
+  }
+</script> -->
+
+<!-- Composition API v2 -->
+
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
-const counter = ref(0)
+// reference
+// const counter = ref(0)
+// const counterTitle = ref('Counter')
+// non-reactive data
+// const inputLabel = 'Edit counter title :'
 
-const increment = () => counter.value++
-const decrement = () => counter.value--
+const counterData = reactive({
+  count: 0,
+  title: 'Counter'
+})
+console.log(counterData)
+const increment = () => counterData.count++
+const decrement = () => counterData.count--
 </script>
 
 <style scoped>
@@ -44,5 +79,9 @@ const decrement = () => counter.value--
 .counter {
   font-size: 40px;
   margin: 10px;
+}
+
+.edit {
+  margin-top: 20px;
 }
 </style>
