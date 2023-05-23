8c9dda8822bcc89e9bf9b92a2d6598e6174123cd
nafisahmed42018 <nafisahmed@iut-dhaka.edu>
2023-05-23 20:23:51 +0600

    pinia store used instead of composable

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -3,14 +3,26 @@ import { defineStore } from 'pinia'
 export const useCounterStore = defineStore({
   id: 'counter',
   state: () => ({
-    counter: 0
+    count: 0,
+    title: 'Counter Title'
   }),
-  getters: {
-    doubleCount: (state) => state.counter * 2
-  },
   actions: {
-    increment() {
-      this.counter++
+    increment(amount) {
+      this.count += amount
+    },
+    decrement(amount) {
+      this.count -= amount
+    }
+  },
+  getters: {
+    oddOrEven() {
+      if (this.count === 0) {
+        return 'unchanged'
+      } else if (this.count % 2 === 0) {
+        return 'even'
+      } else {
+        return 'odd'
+      }
     }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,54 +1,28 @@
 <template>
   <div class="home">
-    <h3>{{ counterData.title }} :</h3>
+    <h3>{{ counter.title }}</h3>
     <div>
-      <button class="btn" @click="decrement(2)">-2</button>
-      <button class="btn" @click="decrement(1)">-</button>
-      <span class="counter">{{ counterData.count }}</span>
-      <button class="btn" @click="increment(1)">+</button>
-      <button class="btn" @click="increment(2, $event)">+2</button>
+      <button class="btn" @click="counter.decrement(2)">-2</button>
+      <button class="btn" @click="counter.decrement(1)">-</button>
+      <span class="counter">{{ counter.count }}</span>
+      <button class="btn" @click="counter.increment(1)">+</button>
+      <button class="btn" @click="counter.increment(2)">+2</button>
     </div>
     <div class="edit">
-      <h4>{{ inputLabel }}</h4>
-      <input v-model="counterData.title" type="text" v-autofocus />
+      <h4>{{ counter.oddOrEven }}</h4>
+      <input v-model="counter.title" type="text" v-autofocus />
     </div>
-    <p>The number on the counter is {{ oddOrEven }}</p>
+    <p>The number on the counter is</p>
   </div>
 </template>
-
-<!-- Options API -->
-<!-- <script>
-export default {
-  data() {
-    return {
-      counter: 0
-    }
-  },
-  methods: {
-    increment() {
-      this.counter++
-    },
-    decrement() {
-      this.counter--
-    }
-  }
-  directives: {
-    autofocus: {
-      mounted(el) {
-        el.focus()
-      }
-    }
-  }
-}
-</script> -->
 
 <!-- Composition API v2 -->
 
 <script setup>
 import { vAutofocus } from '@/directives/vAutofocus'
-import { useCounter } from '../use/useCounter'
+import { useCounterStore } from '@/stores/counter'
 
-const { inputLabel, counterData, oddOrEven, increment, decrement } = useCounter()
+const counter = useCounterStore()
 </script>
 
 <style scoped>
