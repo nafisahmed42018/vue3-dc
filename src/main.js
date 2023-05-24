2d30d3a75b39a6988c65e5ab40b8c55bba8d5e04
nafisahmed42018 <nafisahmed@iut-dhaka.edu>
2023-05-24 17:17:46 +0600

    default design cleaned

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
-import router from './router'
+import ViewNotes from './views/ViewNotes.vue'
+import ViewStats from './views/ViewStats.vue'
 
-const app = createApp(App)
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: ViewNotes,
+    },
+    {
+      path: '/stats',
+      name: 'Stats',
+      component: ViewStats,
+    },
+  ],
+})
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(router).mount('#app')
---
+++ src/views/ViewNotes.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="notes">
+    Notes
+  </div>
+</template>
